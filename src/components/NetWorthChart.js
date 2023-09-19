b575b6d4d3bc208bf99eb043361dae2663c51656
GouravKumar13 <gouravkumarat@gmail.com>
2023-09-19 23:21:26 +0530

    "fix"

--- src/components/NetWorthChart.js
+++ src/components/NetWorthChart.js
@@ -26,46 +26,13 @@ const data02 = [
     { x: 100, y: 100 },
     { x: 120, y: 190 },
 ];
-const data = [
-    {
-        name: 'Current',
-        Account_Receivable: 2450000,
-        Account_Payable: 1340000,
 
 
-    },
-    {
-        name: '1-30',
-        Account_Receivable: 2020000,
-        Account_Payable: 81150,
-
-    },
-    {
-        name: '31-60',
-        Account_Receivable: 801000,
-        Account_Payable: 86560,
-
-    },
-    {
-        name: '61-90',
-        Account_Receivable: 670000,
-        Account_Payable: 79630,
-
-    },
-    {
-        name: '91+',
-        Account_Receivable: 360000,
-        Account_Payable: 66920,
-
-    },
-
-];
 const NetWorthChart = () => {
     return (
         <div>
             <ResponsiveContainer width="100%" height={ 400 }>
                 <ScatterChart
-                    data={ data }
                     margin={ {
                         top: 20,
                         right: 20,
@@ -74,8 +41,8 @@ const NetWorthChart = () => {
                     } }
                 >
                     <CartesianGrid />
-                    <XAxis type="name" dataKey="name" name="stature" />
-                    <YAxis type="number" dataKey="number" name="weight" unit="kg" />
+                    <XAxis type="name" dataKey="x" name="stature" />
+                    <YAxis type="number" dataKey="y" name="weight" unit="kg" />
                     <ZAxis type="number" range={ [100] } />
                     <Tooltip cursor={ { strokeDasharray: '3 3' } } />
                     <Legend />
